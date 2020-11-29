22fa3f2539167e443bd61753a43942dcdc1e6b87
grechka91 <ahrechanychenko@gmail.com>
2020-11-29 21:53:01 +0200

    Delete demo.sh

--- demo.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo 0. Build our build environment and produce image with standalone executable:
-docker build -f build/Dockerfile build -t curl-static
-
-echo -e "\n"
-echo 1 Run tests
-pip3 install pytest lxml
-python3 -m pytest -v tests
