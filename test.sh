9a5907af357c2390b30c9c0fd3ed1651325062bb
grechka91 <ahrechanychenko@gmail.com>
2020-11-29 21:51:13 +0200

    Update test.sh
    add clean-up

--- test.sh
+++ test.sh
@@ -7,5 +7,10 @@ docker build -f build/Dockerfile build -t curl-static
 echo 1. Turn on test environment
 docker-compose up -d
 
+echo 2. Run tests
 pip3 install pytest lxml BeautifulSoup4
 python3 -m pytest -v tests
+
+echo 3. Clean-up
+docker-compose kill
+docker-compose down
